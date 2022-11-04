53d68ee676e09d4fb71fa12426bc7d7161a323f7
bhargav <bhargavguggilapu@gmail.com>
2022-11-05 03:32:41 +0530

    layout issue fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Feed from "./app/components/Feed/Feed";
 import Header from "./app/components/Header/Header";
+import Login from "./app/components/Login/Login";
 import Sidebar from "./app/components/Sidebar/Sidebar";
 import SidebarRight from "./app/components/SidebarRight/SidebarRight";
+import { selectUser } from "./app/Store/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
-    <div className="app">
-      <Header />
-      <div className="body">
-        <Sidebar />
-        <Feed />
-        <SidebarRight />
-      </div>
+    <div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app">
+          <Header />
+          <div className="body">
+            <Sidebar />
+            <Feed />
+            <SidebarRight />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
